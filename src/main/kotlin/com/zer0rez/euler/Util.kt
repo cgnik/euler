817dd36bdfbbafb34925d1a24dbd2be4a54de860
cgnik <christomax@gmail.com>
2019-07-27 00:19:04 -0400

    solved 2 yay

--- src/main/kotlin/com/zer0rez/euler/Util.kt
+++ src/main/kotlin/com/zer0rez/euler/Util.kt
@@ -1,8 +1,9 @@
 package com.zer0rez.euler
 
 object Util {
-    fun swapin(target : IntArray, arrow: Int){
+    fun swapin(target: IntArray, arrow: Int): Int {
         target[0] = target[1]
         target[1] = arrow
+        return target[1]
     }
 }
