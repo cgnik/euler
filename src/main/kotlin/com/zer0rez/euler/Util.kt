3835ba3a8a06e3fc1d32542ba4fa55c9cce7f05f
cgnik <christomax@gmail.com>
2019-07-27 09:03:27 -0400

    small refactor; on to problem3

--- src/main/kotlin/com/zer0rez/euler/Util.kt
+++ src/main/kotlin/com/zer0rez/euler/Util.kt
@@ -6,4 +6,15 @@ object Util {
         target[1] = arrow
         return target[1]
     }
+
+    fun fibonacci(quit: (Int) -> Boolean): ArrayList<Int> {
+        var values = ArrayList<Int>()
+        values.add(1)
+        values.add(2)
+        while (!quit(values.last())) {
+            values.add(values[values.count() - 1] + values[values.count() - 2])
+        }
+        values.remove(values.last())
+        return values
+    }
 }
