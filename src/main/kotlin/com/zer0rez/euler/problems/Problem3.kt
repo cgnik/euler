cc8d15b379de14ed920ea00a0f100a8cdcf648ca
cgnik <christomax@gmail.com>
2019-07-28 22:08:07 -0400

    WIP problem 3

--- src/main/kotlin/com/zer0rez/euler/problems/Problem3.kt
+++ src/main/kotlin/com/zer0rez/euler/problems/Problem3.kt
@@ -3,7 +3,9 @@ package com.zer0rez.euler.problems
 import com.zer0rez.euler.Problem
 import com.zer0rez.euler.Solution
 import kotlin.math.roundToLong
+import kotlin.math.sqrt
 
+fun largestTerm(term: Long) = sqrt(term.toDouble()).roundToLong()
 /**
 https://projecteuler.net/problem=3
 The prime factors of 13195 are 5, 7, 13 and 29.
@@ -12,9 +14,24 @@ What is the largest prime factor of the number 600851475143 ?
 
 class Problem3 : Problem {
     private val term = 600851475143
-    private val half = (term.toDouble() / 2.0).roundToLong()
+
     override fun solve(): Solution {
-        val answer = LongRange(half, 1).firstOrNull { term % it == 0L }
-        return Solution(3, answer.toString(), "")
+        val factors = generateSequence {
+            factor(term)
+        }.toList()
+        val answer = 12
+        return Solution(3, answer.toString(), factors.joinToString(", "))
+    }
+
+    fun factor(num: Long): Sequence<Long?> {
+        var current = largestTerm(num)
+        while (current % 2L == 0L) current /= 2
+        return sequence {
+            while (current > 1) {
+                if (num % current == 0L) yield(current)
+                current -= 2
+            }
+            yield(null)
+        }
     }
 }
