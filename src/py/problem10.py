ba3e483a1fd7e085625bed82d5bcca96e9b4cbd2
Christopher King <christomax@gmail.com>
2019-08-03 13:11:40 -0400

    OO

--- src/py/problem10.py
+++ src/py/problem10.py
@@ -1,30 +1,36 @@
+import threading
+
 from util import divisible_by
 
 
-def find_primes(limit):
-    primes = []
-    for x in range(2, limit):
-        if x % 2 == 0: continue
-        divisors = filter(lambda b: (b <= int(x / 2)), primes)
-        if not divisible_by(x, *divisors):
-            primes.append(x)
-            yield x
+class Primerator:
+    def __init__(self, limit):
+        self.lock = threading.Lock()
+        self.limit = limit
+
+    def find_primes(self, limit):
+        primes = []
+        for x in range(2, limit):
+            if x % 2 == 0: continue
+            divisors = filter(lambda b: (b <= int(x / 2)), primes)
+            if not divisible_by(x, *divisors):
+                primes.append(x)
+                yield x
+
+    def status(self, primes):
+        with self.lock:
+            if len(primes) % 100 == 0:
+                print('.', end='')
+            if len(primes) % 1000 == 0:
+                print(f"latest: {primes[-1]}")
+
+    def problem10(self):
+        primes = []
+        for prime in self.find_primes(self.limit):
+            primes.append(prime)
+            self.status(primes)
+        print(f"\nAnswer: {sum(primes)}")
 
 
-def status(primes):
-    if len(primes) % 100 == 0:
-        print('.', end='')
-    if len(primes) % 1000 == 0:
-        print(f"latest: {primes[-1]}")
-
-
-def problem10():
-    limit = 2000000
-    primes = []
-    for prime in find_primes(limit):
-        primes.append(prime)
-        status(primes)
-    print(f"\nPrimes: {primes}\nAnswer: {sum(primes)}")
-
-
-problem10()
+# 2000000
+Primerator(20000).problem10()
