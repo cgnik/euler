696612ba673e85df295487e65134b51be7fe3f06
Christopher King <christomax@gmail.com>
2019-08-18 21:15:35 -0400

    some tests to have fun

--- src/py/util/test_grids.py
+++ src/py/util/test_grids.py
@@ -14,7 +14,18 @@ class Test_grids(TestCase):
         self.assertCountEqual(get_sticks(np.array(self.test_data1)), [])
         self.assertCountEqual(get_sticks(np.array(self.test_data2)), [(1, 2), (1, 3), (2, 4), (3, 4)])
         self.assertCountEqual(get_sticks(np.array(self.test_data3)), [(1, 2), (1, 4), (2, 5), (2, 3), (3, 6),
-                                                                      (4, 5), (4, 7), (5, 6), (5, 8), (6, 9), (7, 8), (8, 9)])
+                                                                      (4, 5), (4, 7), (5, 6), (5, 8), (6, 9), (7, 8),
+                                                                      (8, 9)])
+
+    def test_20_sticks(self):
+        # it's always 2(n^2) - 2n
+        def sickerize(n):
+            arr = np.arange(n ** 2).reshape((n, n))
+            x = get_sticks(arr)
+            print(f"{n} sticks: {len(x)}\n\tX :: {x}")
+
+        for n in [2, 3, 6, 7, 8, 9, 20]:
+            sickerize(n)
 
     def test_gen_paths_sanity(self):
         self.assertListEqual(gen_paths(None), [])
