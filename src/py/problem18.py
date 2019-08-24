1f592b8c8cd165e6691e95a0e12df6ce1b66a920
Christopher King <christomax@gmail.com>
2019-08-24 10:20:54 -0400

    upwards calculations; need weights now for more than value pathing

--- src/py/problem18.py
+++ src/py/problem18.py
@@ -1,27 +1,12 @@
 from statistics import mean
 
-data = [list(map(lambda x: int(x), a.strip().split(' '))) for a in """75
-                          95 64
-                        17 47 82
-                      18 35 87 10
-                    20 04 82 47 65
-                  19 01 23 75 03 34
-                88 02 77 73 07 63 67
-              99 65 04 28 06 16 70 92
-            41 41 26 56 83 40 80 70 33
-          41 48 72 33 47 32 37 16 94 29
-        53 71 44 65 25 43 91 52 97 51 14
-      70 11 33 28 77 73 17 78 39 68 17 57
-    91 71 52 38 17 14 91 43 58 50 27 29 48
-  63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23""".split("\n")]
 
 
 def weight(d, row, column):
-    w = [d for i in range(0, len(data) - row) for d in data[i][column:column + i]]
+    w = [d for i in range(0, len(d) - row) for d in d[i][column:column + i]]
     if len(w) == 0:
-        return data[row][column]
-    return int(mean([data[row][column], mean(w)]))
+        return d[row][column]
+    return int(mean([d[row][column], mean(w)]))
 
 
 def weight_data(d):
@@ -49,37 +34,53 @@ def pathize(tiers, override={}):
     return indices
 
 
-def nextmax(nums, notindices):
-    n = nums.copy()
-    n.sort(reverse=True)
-    count = 0
-    idx = None
-    while not idx or idx in notindices:
-        idx = nums.index(n[count])
-        count += 1
-    return idx
+def next_index(this_index, nums):
+    ln = len(nums)
+    if this_index == 0 or ln == 1:
+        return 0
+    elif this_index >= ln:
+        return ln -1
+    elif nums[this_index] < nums[this_index - 1]:
+        return this_index - 1
+    return this_index
 
 
-def pathize_up(tiers):
-    indices = []
-    lastidx = None
-    for i in reversed(range(0, len(tiers))):
-        notindices = []
-        for j in range(0, len(tiers[i])):
-            maxidx = nextmax(tiers[i], notindices)
-            if not lastidx or maxidx == lastidx or maxidx == lastidx - 1:
-                break
-        indices.append(maxidx)
-        lastidx = maxidx
-    indices.reverse()
-    return indices
+def pathize_up(triangle):
+    tiers = triangle.copy()
+    tiers.reverse()
+    max_path = [0]
+    for num_index, num in enumerate(tiers[0]):
+        path = [(num_index, num)]
+        for tier_index, tier in enumerate(tiers):
+            if tier_index == 0: continue
+            up_index = next_index(path[-1][0], tier)
+            path.append((up_index, tier[up_index]))
+        addends = [x[1] for x in path]
+        if sum(addends) > sum(max_path):
+            max_path = addends
+    max_path.reverse()
+    return max_path
 
 
-def problem18(d):
-    answer_indices = pathize_up(d)
-    # answer = [r[answer_indices[rindex]] for rindex, r in enumerate(d)]
-    answer = [r[answer_indices[rindex]] for rindex, r in enumerate(d)]
+def problem18():
+    d = [list(map(lambda x: int(x), a.strip().split(' '))) for a in """75
+                              95 64
+                            17 47 82
+                          18 35 87 10
+                        20 04 82 47 65
+                      19 01 23 75 03 34
+                    88 02 77 73 07 63 67
+                  99 65 04 28 06 16 70 92
+                41 41 26 56 83 40 80 70 33
+              41 48 72 33 47 32 37 16 94 29
+            53 71 44 65 25 43 91 52 97 51 14
+          70 11 33 28 77 73 17 78 39 68 17 57
+        91 71 52 38 17 14 91 43 58 50 27 29 48
+      63 66 04 68 89 53 67 30 73 16 69 87 40 31
+    04 62 98 27 23 09 70 98 73 93 38 53 60 04 23""".split("\n")]
+
+    answer = pathize_up(d)
     print(f"Answer: {sum(answer)} :: {answer}")
 
 
-problem18(data)
+problem18()
