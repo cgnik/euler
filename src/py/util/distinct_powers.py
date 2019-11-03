ded146e82f22771ae0c34622f6da1e63b1d9e78c
Christopher King <christomax@gmail.com>
2019-11-02 21:02:28 -0400

    still working on it... sigh

--- src/py/util/distinct_powers.py
+++ src/py/util/distinct_powers.py
@@ -3,17 +3,17 @@ import itertools as it
 import numpy as np
 
 # NEED TO DEDUPE/SIMPLIFY_POWERS ON THE POWS LIST BEFORE USING IT. CHICKENEGG.
-pows = {a ** b: (a, b) for b in range(2, 10) for a in range(2, 10)}
+pows = {a ** b: (a, b) for b in range(2, 10) for a in range(2, 11) if a ** b <= 100}
 print(f"POWERS: {pows}")
 
 
 def distinct_powers(l, h):
     values = np.arange(l, h + 1)
-    terms = {simplify_powers(*p) for p in it.product(values, values)}
+    terms = {simplify_exponent(*p) for p in it.product(values, values)}
     print(f"terms {terms}")
     return len(terms)
 
 
-def simplify_powers(base, pow):
+def simplify_exponent(base, pow):
     multiplier = pows.get(base, (1, 1))
-    return int(base ** (1 / multiplier[1])), pow * multiplier[1]
+    return int(base ** (1 / multiplier[1])), pow + multiplier[1]
--- src/py/util/test_distinct_powers.py
+++ src/py/util/test_distinct_powers.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from util.distinct_powers import distinct_powers, simplify_powers
+from util.distinct_powers import distinct_powers, simplify_exponent
 
 
 class Test_distinct_powers(TestCase):
@@ -8,8 +8,9 @@ class Test_distinct_powers(TestCase):
         # self.assertSetEqual(distinct_powers(2, 5), {4, 8, 9, 16, 25, 27, 32, 64, 81, 125, 243, 256, 625, 1024, 3125})
         self.assertEqual(distinct_powers(2, 5), 15)
 
-    def test_simplify_powers(self):
-        self.assertEqual(simplify_powers(4, 2), (2, 4))
-        self.assertEqual(simplify_powers(9, 2), (3, 4))
-        self.assertEqual(simplify_powers(16, 2), (2, 8))
-        self.assertEqual(simplify_powers(2, 2), (2, 2))
+    def test_simplify_exponent(self):
+        self.assertEqual(simplify_exponent(4, 2), (2, 4))
+        self.assertEqual(simplify_exponent(9, 2), (3, 4))
+        self.assertEqual(simplify_exponent(16, 2), (2, 8))
+        self.assertEqual(simplify_exponent(2, 2), (2, 2))
+        self.assertEqual(simplify_exponent(7, 7), (7, 7))
